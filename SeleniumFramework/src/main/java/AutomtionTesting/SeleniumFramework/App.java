82ed73d13623829be739d516eabd9ef1f03fa4e5
shrutichopra91 <shruti.91chopra@gmail.com>
2023-12-24 14:33:17 +0530

    initial stage

---
+++ SeleniumFramework/src/main/java/AutomtionTesting/SeleniumFramework/App.java
@@ -0,0 +1,13 @@
+package AutomtionTesting.SeleniumFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
